61bb35ef913c4ee315ccde5a162d30ecbb618846
Whiff <Jakub.struska@creativehill.cz>
2024-04-15 21:31:32 +0200

    14 mono one again

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin:['http://localhost:3000', "maturita.railway.internal","https://maturita.railway.internal", "https://maturita-production.up.railway.app"],
+  origin:['http://localhost:3000', "https://limber-coal-production.up.railway.app"],
   credentials: true
 }));
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:['http://localhost:3000', "maturita.railway.internal","https://maturita.railway.internal","https://maturita-production.up.railway.app"],
+    origin:['http://localhost:3000',"https://limber-coal-production.up.railway.app"],
     credentials: true
   }
 })
