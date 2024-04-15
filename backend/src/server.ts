5ace3893657d24714b1ea73e4d7cccf5908b12a6
Whiff <Jakub.struska@creativehill.cz>
2024-04-15 14:48:26 +0200

    KYS

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,13 +7,15 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(cors())
+app.use(cors({
+  origin: ["uppity-pig-production.up.railway.app", "http://localhost:3000"]
+}))
 const server = createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    }
+  cors: {
+    origin: ["uppity-pig-production.up.railway.app", "http://localhost:3000"]
+  }
 })
 
 
