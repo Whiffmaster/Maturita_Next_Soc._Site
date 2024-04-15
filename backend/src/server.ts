280dd849c940f0bfac463f4d77893670d6f11118
Whiff <Jakub.struska@creativehill.cz>
2024-04-15 18:52:37 +0200

    localhost?? 3

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,14 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin:['http://localhost:3000', "https://maturita-production.up.railway.app", "::1", "http://localhost::1"],
+  origin:['http://localhost:3000', "maturita.railway.internal"],
   credentials: true
 }));
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin:['http://localhost:3000', "https://maturita-production.up.railway.app", "::1", "http://localhost::1"],
+    origin:['http://localhost:3000', "maturita.railway.internal"],
     credentials: true
   }
 })
