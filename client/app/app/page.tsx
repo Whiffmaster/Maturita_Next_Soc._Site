b3cf2e46cfd86c489e2d6663ecba78b5f521ec74
Whiff <Jakub.struska@creativehill.cz>
2024-04-15 19:05:33 +0200

    localhost?? 4

--- client/app/app/page.tsx
+++ client/app/app/page.tsx
@@ -65,7 +65,7 @@ export default function HomePage() {
         console.log(res)
         setActiveFriends(res)
       })
-      setSocket(io(process.env.NEXT_PUBLIC_REACT_APP_SOCKET_URL ||"uppity-pig.railway.internal", {auth: {userID: data?.user?.id}}))
+      setSocket(io(process.env.NEXT_PUBLIC_REACT_APP_SOCKET_URL ||"uppity-pig.railway.internal:5000", {auth: {userID: data?.user?.id}}))
       console.log("listening to: "+ process.env.NEXT_PUBLIC_REACT_APP_SOCKET_URL)
     }).catch((err) => {
       console.log(err)
