c65d3db5b17ffb278a94fe3c15899c79e4ddab89
Whiff <Jakub.struska@creativehill.cz>
2024-04-15 18:34:03 +0200

    localhost??

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,16 +8,14 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-  origin: "127.0.0.1",
-  methods: ["GET", "POST"],
+  origin:'http://localhost:3000',
   credentials: true
 }));
 const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "127.0.0.1",
-    methods: ["GET", "POST"],
+    origin:'http://localhost:3000',
     credentials: true
   }
 })
